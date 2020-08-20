cd9aa015384b0e876c9b77f46f7607af043035c4
Julia0K <yulia.kolesnyk@gmail.com>
2020-08-20 13:23:14 -0700

    homework

--- src/com/ykolesnyk/Main.java
+++ src/com/ykolesnyk/Main.java
@@ -3,40 +3,48 @@ package com.ykolesnyk;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-            //TO DO:
-            // Save Contacts
-            // Name
-            // Last name
-            // Phone number
-            // Address
-            // Date of birth
-            // Search by phone
-            // Search query input
-            // Display search results in console
+        //TO DO:
+        // Save Contacts
+        // Name
+        // Last name
+        // Phone number
+        // Address
+        // Date of birth
+        // Search by phone
+        // Search query input
+        // Display search results in console
         Address address = new Address();
         address.setHouseNumber(100);
-        //address.getHouseNumber();
         System.out.println(address.getHouseNumber());
-        //textInput();
-        createContact(textInput(),textInput());
+        String firstName = textInput("first name");
+        String lastName = textInput("last name");
+        long phoneNumber = Long.parseLong(textInput("phone number"));
+        Date date = textInput("date if birth");
+        createContact(firstName, lastName, phoneNumber, date);
     }
-    public static Contact createContact(String firstName, String lastName) {
-        Contact contact = new Contact();
+
+    public static Contact createContact(String firstName, String lastName, long phoneNumber, Date date) { // <--- all this row is method signature
+        Contact contact = new Contact(); // <--- Creates new object Contact
         contact.setFirstName(firstName);
         contact.setLastName(lastName);
-        System.out.println(contact.getFirstName() + contact.getLastName());
+        contact.setPhoneNumber(phoneNumber);
+        contact.setDate(Date.date);
+        System.out.println(contact.getFirstName() +"\n"+ contact.getLastName() + "\n"+ contact.getPhoneNumber() + "\n" + contact.getDate());
 
         return contact;
     }
-    public static String textInput() throws IOException {
+
+    public static String textInput(String message) throws IOException {
         InputStreamReader streamReader = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(streamReader);
+        System.out.println("Please enter your " + message);
         String text = reader.readLine();
-        System.out.println(text);
+        System.out.println("You entered " + text + " as your " + message);
 
         return text;
 
